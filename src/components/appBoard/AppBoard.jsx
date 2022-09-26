6970d8dff83b95fac62d0476ea1b36be81a5e879
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-09-26 18:21:21 +0300

    Optimization

--- src/components/appBoard/AppBoard.jsx
+++ src/components/appBoard/AppBoard.jsx
@@ -1,9 +1,55 @@
 import { Component } from "react";
-import AppBoardToDo from "../appBoardToDo/AppBoardToDo";
-import AppBoardInProgress from "../appBoardInProgress/AppBoardInProgress";
-import AppBoardDone from "../appBoardDone/AppBoardDone";
 
 import "./appBoard.sass"
+
+class BoardStatus extends Component {
+    onStatusChange = (id) => {
+        this.props.onStatusChange(id)
+    }
+
+    renderTasks = (arr) => {
+        const tasks = arr.map((a, i) => {
+            if (a.completed === this.props.status) {
+                return (
+                    <div
+                        key={a.id}
+                        onClick={() => this.onStatusChange(i)}
+                        className="board__column__item">
+    
+                        <div
+                            style={this.props.bgColors[i]}
+                            className="board__column__item-initials">{this.props.users[i]}</div>
+    
+                        <div className="board__column__item-title">{a.title}</div>
+                    </div>
+                )
+            }
+        })
+
+        return (
+            <div className="board__column__content">
+                {tasks}
+            </div>
+        )
+
+    }
+
+    render() {
+        return (
+            <>
+                <div className="board__column">
+                    <h2 className="board__column__heading">To do</h2>
+
+                    {this.renderTasks(this.props.tasks)}
+                </div>
+            </>
+        )
+    }
+}
+
+class AppBoardToDo extends BoardStatus {}
+class AppBoardInProgress extends BoardStatus {}
+class AppBoardDone extends BoardStatus {}
 
 class AppBoard extends Component {
     render() {
@@ -15,6 +61,7 @@ class AppBoard extends Component {
                     bgColors={bgColors}
                     users={users}
                     tasks={tasks}
+                    status={"To do"}
                     onStatusChange={this.props.onStatusChange}
                      />
 
@@ -22,6 +69,7 @@ class AppBoard extends Component {
                     bgColors={bgColors}
                     users={users}
                     tasks={tasks}
+                    status={"In progress"}
                     onStatusChange={this.props.onStatusChange}
                      />
 
@@ -29,6 +77,7 @@ class AppBoard extends Component {
                     bgColors={bgColors}
                     users={users}
                     tasks={tasks}
+                    status={"Done"}
                     onStatusChange={this.props.onStatusChange}
                      />
             </div>
